f893985015478eda9eea975f425475833bd4163b
dkornel01 <kornel.dobrocsi@gmail.com>
2023-03-02 16:05:55 +0100

    kesz

--- SzamolosFeladatok/src/tombbel/DobasKettoKockaval.java
+++ SzamolosFeladatok/src/tombbel/DobasKettoKockaval.java
@@ -5,13 +5,13 @@ import java.util.Random;
 public class DobasKettoKockaval {
 
     private static Random rnd = new Random();
-    
+
     public static void main(String[] args) {
         /* 1 lehetséges kimenet 5 dobá esetén */
 //        System.out.println(" 5 ****** (2 db)");
 //        System.out.println("11 *** (1 db)");
 //        System.out.println("12 ****** (2 db)");
-        
+
         int[] dobasok = kettoKockavalDob(5);
         statisztika(dobasok);
     }
@@ -19,7 +19,7 @@ public class DobasKettoKockaval {
     private static int[] kettoKockavalDob(int db) {
         int[] dobasok = new int[13];
         for (int i = 0; i < db; i++) {
-            int dobas = rnd.nextInt(2,13);
+            int dobas = rnd.nextInt(2, 13);
             dobasok[dobas]++; //dobasok[dobas] = dobasok[dobas] + 1;
         }
         return dobasok;
@@ -31,7 +31,7 @@ public class DobasKettoKockaval {
         for (int i = 0; i < adatok.length; i++) {
             if (adatok[i] != 0) {
                 System.out.printf("%2d ", i); //index
-                double arany = (double)adatok[i] / maxElofordulas;
+                double arany = (double) adatok[i] / maxElofordulas;
                 egysor(arany);
                 System.out.printf(" (%d db)\n", adatok[i]);
             }
@@ -41,7 +41,7 @@ public class DobasKettoKockaval {
     private static int maximumKivalasztas(int[] adatok) {
         int maxIndex = 0;
         for (int i = 0; i < adatok.length; i++) {
-            if(adatok[i] > adatok[maxIndex]){
+            if (adatok[i] > adatok[maxIndex]) {
                 maxIndex = i;
             }
         }
@@ -56,5 +56,5 @@ public class DobasKettoKockaval {
         }
         //System.out.println("");
     }
-    
+
 }
--- src/szamolasok/Szamolasok.java
+++ src/szamolasok/Szamolasok.java
@@ -1,26 +1,16 @@
-
 package szamolasok;
-
 
 public class Szamolasok {
 
-    
     public static void main(String[] args) {
-    int[] szamok={2,3,4};
-    String megold="";
-    int elsoszam=0;
-    int masodikszam=0;
-    int harmadikszam=0;
-    for (int i=0;i<szamok.length;i++){
-         elsoszam=szamok[i];
-        for (int x=0;x<szamok.length;x++){
-             masodikszam=szamok[x];
-            for (int y=0;y<szamok.length;y++){
-                 harmadikszam=szamok[y];
-                System.out.println(szamok[i]+""+szamok[x]+""+szamok[y]);
+        int[] szamok = {2, 3, 4};
+        for (int i = 0; i < szamok.length; i++) { 
+            for (int x = 0; x < szamok.length; x++) { 
+                for (int y = 0; y < szamok.length; y++) {
+                    System.out.println(szamok[i] + "" + szamok[x] + "" + szamok[y]);
+                }
             }
         }
-       }
     }
-    
+
 }
